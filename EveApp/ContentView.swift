f72fd60dbd2ce75d810620cf48ac02a363eceec6
Erik Hatfield <ehhatfield@protonmail.com>
2023-05-24 21:03:30 -0500

    Initial Commit

---
+++ EveApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  EveApp
+//
+//  Created by Erik Hatfield on 5/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EveApp/EveAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EveAppApp.swift
+//  EveApp
+//
+//  Created by Erik Hatfield on 5/24/23.
+//
+
+import SwiftUI
+
+@main
+struct EveAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

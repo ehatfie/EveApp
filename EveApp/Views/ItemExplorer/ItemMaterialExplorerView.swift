b9586975a08694cf58d13b3e3304fc27c912337e
Erik Hatfield <ehhatfield@protonmail.com>
2023-10-20 22:43:35 -0500

    Initial ItemMaterialExplorer

---
+++ EveApp/Views/ItemExplorer/ItemMaterialExplorerView.swift
@@ -0,0 +1,18 @@
+//
+//  ItemMaterialExplorerView.swift
+//  EveApp
+//
+//  Created by Erik Hatfield on 10/20/23.
+//
+
+import SwiftUI
+
+struct ItemMaterialExplorerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemMaterialExplorerView()
+}

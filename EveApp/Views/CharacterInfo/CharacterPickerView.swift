73f67217e6c13493ffef192f8ae32ba2b2ef6e98
Erik Hatfield <erikhatfield@Eriks-Mac-mini.local>
2024-12-14 22:30:51 -0600

    paste to keyboard

---
+++ EveApp/Views/CharacterInfo/CharacterPickerView.swift
@@ -0,0 +1,18 @@
+//
+//  CharacterPickerView.swift
+//  EveApp
+//
+//  Created by Erik Hatfield on 12/14/24.
+//
+
+import SwiftUI
+
+struct CharacterPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CharacterPickerView()
+}
